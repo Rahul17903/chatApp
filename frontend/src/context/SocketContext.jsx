1b03ded5156575bd48b2e381c5bd65a50cebfba3
Rahul Ghosh <rahulghosh17903@gmail.com>
2025-11-24 01:14:22 +0530

    add new frontend link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser} = useAuth();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5173/",{
+            const socket = io("https://chatapp-p6f2.onrender.com/",{
                 query:{
                     userId:authUser?._id,
                 }
